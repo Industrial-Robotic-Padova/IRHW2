87cc44319e683c9dffbc61ff26c69598b10bf4a1
sepideh <sepideh.shamsizadeh@gmail.com>
2022-01-11 18:04:27 +0100

    robot_move

--- tiago_iaslab_simulation/src/robot_control.py
+++ tiago_iaslab_simulation/src/robot_control.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 import rospy
 
@@ -19,6 +20,10 @@ class PointHeadClient(object):
         rospy.loginfo("Waiting for '/play_motion' AS...")
         self.head_cmd = rospy.Publisher(
             '/head_controller/command', JointTrajectory, queue_size=1.0)
+        self.torso_cmd = rospy.Publisher(
+            '/torso_controller/command', JointTrajectory, queue_size=1.0)
+        self.arm_cmd = rospy.Publisher(
+            '/arm_controller/command', JointTrajectory, queue_size=1.0)
         rospy.loginfo("Moving head down")
         self.play_m_as = SimpleActionClient('/play_motion', PlayMotionAction)
         if not self.play_m_as.wait_for_server(rospy.Duration(20)):
@@ -27,21 +32,44 @@ class PointHeadClient(object):
         rospy.loginfo("Connected!")
         rospy.sleep(1.0)
         rospy.loginfo("Done initializing PickAruco.")
-        self.look_at(2.0, 0.0, 0.6, "base_link")
+        self.move_head()
+        self.move_torso()
+        self.open_arm()
         pmg = PlayMotionGoal()
         pmg.motion_name = 'pick_final_pose'
         pmg.skip_planning = False
         self.play_m_as.send_goal_and_wait(pmg)
         rospy.loginfo("Raise object done.")
 
-    def look_at(self, x, y, z, frame, duration=1.0):
+    def move_head(self):
         jt = JointTrajectory()
         jt.joint_names = ['head_1_joint', 'head_2_joint']
         jtp = JointTrajectoryPoint()
-        jtp.positions = [x, z]
+        jtp.positions = [0.0, -0.75]
         jtp.time_from_start = rospy.Duration(2.0)
         jt.points.append(jtp)
         self.head_cmd.publish(jt)
+        rospy.loginfo("Done.")
+
+    def move_torso(self):
+        rospy.loginfo("Moving torso up")
+        jt = JointTrajectory()
+        jt.joint_names = ['torso_lift_joint']
+        jtp = JointTrajectoryPoint()
+        jtp.positions = [0.34]
+        jtp.time_from_start = rospy.Duration(2.5)
+        jt.points.append(jtp)
+        self.torso_cmd.publish(jt)
+
+    def open_arm(self):
+        jt = JointTrajectory()
+        jt.joint_names = ['arm_1_joint', 'arm_2_joint', 'arm_3_joint', 'arm_4_joint', 'arm_5_joint', 'arm_6_joint', 'arm_7_joint']
+        jtp = JointTrajectoryPoint()
+        jtp.positions = [2.5, 0.2, -2.1, 1.9, 1.0, -0.5, 0.0]
+        #jtp.positions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        jtp.time_from_start = rospy.Duration(2.0)
+        jt.points.append(jtp)
+        self.arm_cmd.publish(jt)
         rospy.loginfo("Done.")
 
 
--- tiago_iaslab_simulation/src/send_goal_client.py
+++ tiago_iaslab_simulation/src/send_goal_client.py
@@ -3,7 +3,6 @@ import rospy
 import time
 import actionlib
 from move_base_msgs import msg
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 
 def feedback_callback(feedback):
@@ -33,25 +32,4 @@ while True:
     # sends the goal to the action server
     client.send_goal(goal, feedback_cb=feedback_callback)
     client.wait_for_result()
-    head_cmd = rospy.Publisher(
-            '/head_controller/command', JointTrajectory, queue_size=1.0)
-    jt = JointTrajectory()
-    print("head low")
-    jt.joint_names = ['head_1_joint', 'head_2_joint']
-    jtp = JointTrajectoryPoint()
-    jtp.positions = [0, -0.75]
-    jtp.time_from_start = rospy.Duration(2.0)
-    jt.points.append(jtp)
-    head_cmd.publish(jt)
-    rospy.loginfo("Done.")
-    print('[Result] State: %d' % (client.get_state()))
-    torso_cmd = rospy.Publisher(
-            '/torso_controller/command', JointTrajectory, queue_size=1)
-    rospy.loginfo("Moving torso up")
-    jt = JointTrajectory()
-    jt.joint_names = ['torso_lift_joint']
-    jtp = JointTrajectoryPoint()
-    jtp.positions = [0.34]
-    jtp.time_from_start = rospy.Duration(2.5)
-    jt.points.append(jtp)
-    torso_cmd.publish(jt)
+
