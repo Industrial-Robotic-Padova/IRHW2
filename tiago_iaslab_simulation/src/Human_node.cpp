ae11c426ff696a8f36415da815113ebb92e8e566
sepideh-shamsizadeh <sepideh.shamsizadeh@studenti.unipd.it>
2022-01-12 16:03:16 +0100

    Service: start and launch file 3

--- tiago_iaslab_simulation/src/Human_node.cpp
+++ tiago_iaslab_simulation/src/Human_node.cpp
@@ -2,7 +2,7 @@
 #include <tiago_iaslab_simulation/Human.h>
 
 int main(int argc, char **argv){
-  ros::init(argc, argv, "human_node");
+  ros::init(argc, argv, "Human_node");
   auto nh_ptr = std::make_shared<ros::NodeHandle>();
 
   Human human(nh_ptr);
