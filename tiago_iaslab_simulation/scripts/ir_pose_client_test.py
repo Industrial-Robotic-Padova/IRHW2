706140b4545435c04135f2a46e593d16b737f361
gkiavash <gkiavash@gmail.com>
2022-01-12 16:26:13 +0100

    Service: get datat in code

--- tiago_iaslab_simulation/scripts/ir_pose_client_test.py
+++ tiago_iaslab_simulation/scripts/ir_pose_client_test.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python
+from __future__ import print_function
+
+import sys
 import rospy
 import actionlib
 
 from tiago_iaslab_simulation.msg import IRMoveAction, IRMoveGoal, IRMoveFeedback, IRMoveResult
+from tiago_iaslab_simulation.srv import Objs
 
 
 def ir_pose_client(pos):
@@ -15,10 +19,23 @@ def ir_pose_client(pos):
     return client.get_result()
 
 
+def get_obj_ids():
+    try:
+        rospy.wait_for_service('human_objects_srv')
+        get_ids = rospy.ServiceProxy('human_objects_srv', Objs)
+        res = get_ids(1, 1)
+        return res.ids
+
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+
 if __name__ == '__main__':
     try:
         rospy.init_node('ir_pose_client_test')
         result = ir_pose_client([8, 0, 0])
         print("Result:", result.status)
+        result = get_obj_ids()
+        print(result)
+
     except rospy.ROSInterruptException:
         print("program interrupted before completion")
