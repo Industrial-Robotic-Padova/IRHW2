17d8a334d84b85c0ff70cab1bd0645590ae1c45c
gkiavash <gkiavash@gmail.com>
2022-01-13 14:44:03 +0100

    Service: fixed, move all tables

--- tiago_iaslab_simulation/scripts/ir_pose_client_test.py
+++ tiago_iaslab_simulation/scripts/ir_pose_client_test.py
@@ -8,8 +8,15 @@ import actionlib
 from tiago_iaslab_simulation.msg import IRMoveAction, IRMoveGoal, IRMoveFeedback, IRMoveResult
 from tiago_iaslab_simulation.srv import Objs
 
+positions_ = {
+    'table': (8, -2, -0.68),
+    '1': (10.5, 0.45, -0.9),
+    '2': (11.5, 0.45, -0.9),
+    '3': (12.5, 0.45, -0.9),
+}
 
-def ir_pose_client(pos):
+
+def send_pose(pos):
     client = actionlib.SimpleActionClient('/ir_pose', IRMoveAction)
     client.wait_for_server()
 
@@ -29,13 +36,19 @@ def get_obj_ids():
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
+
 if __name__ == '__main__':
     try:
         rospy.init_node('ir_pose_client_test')
-        result = ir_pose_client([8, 0, 0])
+        result = send_pose([8, 0, 0])
         print("Result:", result.status)
-        result = get_obj_ids()
-        print(result)
+        ids_ = get_obj_ids()
+        print("ids:", result)
+        for id_ in ids_:
+            send_pose(positions_.get('table'))
+            # pick
+            send_pose(positions_.get(str(id_)))
+            # put
 
     except rospy.ROSInterruptException:
         print("program interrupted before completion")
