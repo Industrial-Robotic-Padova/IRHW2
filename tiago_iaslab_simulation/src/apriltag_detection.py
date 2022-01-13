5b128463c2770bc77bcf2d9c9925814f961e27f7
sepideh <sepideh.shamsizadeh@gmail.com>
2022-01-13 10:57:48 +0100

    postion with apriltag

--- tiago_iaslab_simulation/src/apriltag_detection.py
+++ tiago_iaslab_simulation/src/apriltag_detection.py
@@ -6,11 +6,10 @@ from  apriltag_ros.msg import AprilTagDetectionArray
 
 
 def tag_callback(msg):
-
-    print('FINAL POSES: ', msg.detections[0].id)
+    print('ID:',  msg.detections[0].id)
+    print('FINAL POSES: ', msg.detections[0].pose.pose.pose.position)
 
 
 rospy.init_node('tag_detection_april')
 tag_msg = rospy.Subscriber('/tag_detections', AprilTagDetectionArray, tag_callback)
 rospy.spin()
-
