dd2a8619c4bd1e92cd5880a11c0f1c87bdf3d1d7
sepideh-shamsizadeh <sepideh.shamsizadeh@studenti.unipd.it>
2022-01-18 13:32:27 +0100

    apriltag test

--- tiago_iaslab_simulation/scripts/node_camera.py
+++ tiago_iaslab_simulation/scripts/node_camera.py
@@ -4,6 +4,7 @@ import rospy
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from apriltag_ros.msg import AprilTagDetectionArray
 
 
 def image_callback(img_msg):
@@ -17,12 +18,18 @@ def image_callback(img_msg):
         rospy.logerr("CvBridge Error: {0}".format(e))
 
 
+def tag_callback(msg):
+    print(msg)
+    print('FINAL POSES: ', msg.detections[0].pose.pose.pose.position)
+
+
 if __name__ == "__main__":
     rospy.init_node('camera', anonymous=True)
     rospy.loginfo("node camera stating...")
     bridge = CvBridge()
 
     sub_image = rospy.Subscriber("/xtion/rgb/image_raw", Image, image_callback)
+    sub_msg = rospy.Subscriber('/tag_detections', AprilTagDetectionArray, tag_callback)
 
     cv2.namedWindow("Image Window", 1)
 
