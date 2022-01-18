614ec73d32c1db8ce69e7522cc4a319931446a59
sepideh-shamsizadeh <sepideh.shamsizadeh@studenti.unipd.it>
2022-01-19 00:53:53 +0100

    check each side table

--- tiago_iaslab_simulation/scripts/pose_client_test.py
+++ tiago_iaslab_simulation/scripts/pose_client_test.py
@@ -1,58 +1,12 @@
 #! /usr/bin/env python
 from __future__ import print_function
 
-import sys
 import rospy
 import actionlib
 
 from tiago_iaslab_simulation import msg as ir_msg
 from tiago_iaslab_simulation.srv import Objs
-
-positions_ = {
-    'table': (8, -2, -0.68),
-    '1': (10.5, 0.75, -0.9),
-    '2': (11.5, 0.75, -0.9),
-    '3': (12.5, 0.75, -0.9),
-}
-coords = {
-    'base': (-6.580047, 1.369940),
-    'table': (1.245143, -1.613171),
-    '1': (4.007396, 1.015966),
-    '2': (5.007404, 1.015966),
-    '3': (6.007146, 1.015966),
-}
-table_size = 0.913
-cylinder_size = 0.21
-robot_size = 0.7
-
-
-def pose_calc(obj_str):
-    return coords.get(obj_str)[0] - coords.get('base')[0], coords.get(obj_str)[1] - coords.get('base')[1]
-
-
-def pose_calc_cyl(obj_str):
-    pose_table = list(pose_calc(obj_str))
-    pose_table[1] -= (cylinder_size + robot_size/2)
-    pose_table.append(0.68)
-    return tuple(pose_table)
-
-
-def pose_calc_table(angle):
-    pose_table = list(pose_calc('table'))
-    if angle == 1:
-        pose_table[0] += table_size
-        pose_table.append(-0.68)
-    if angle == 2:
-        pose_table[0] -= table_size
-        pose_table.append(0)
-    if angle == 3:
-        pose_table[1] += table_size
-        pose_table.append(0)
-    if angle == 4:
-        pose_table[1] -= table_size
-        pose_table.append(0)
-
-    return tuple(pose_table)
+from utils import *
 
 
 def send_pose(pos):
@@ -96,9 +50,17 @@ if __name__ == '__main__':
         print("Result:", result.status)
         ids_ = get_obj_ids()
         print("ids:", result)
+
+        # Robot stand up ....
+        pass
+
         for id_ in ids_:
-            send_pose(pose_calc_table(angle=1))
-            obj_pos = get_obj_pose(id_)
+            # Do for each object
+
+            # Check on the table:
+            for angle in [1, 2, 3, 4]:
+                send_pose(pose_calc_table(angle=angle))
+                obj_pos = get_obj_pose(id_)
             print(obj_pos)
             # pick
             # send_pose(pose_calc_cyl(str(id_)))
---
+++ tiago_iaslab_simulation/scripts/utils.py
@@ -0,0 +1,45 @@
+positions_ = {
+    'table': (8, -2, -0.68),
+    '1': (10.5, 0.75, -0.9),
+    '2': (11.5, 0.75, -0.9),
+    '3': (12.5, 0.75, -0.9),
+}
+coords = {
+    'base': (-6.580047, 1.369940),
+    'table': (1.245143, -1.613171),
+    '1': (4.007396, 1.015966),
+    '2': (5.007404, 1.015966),
+    '3': (6.007146, 1.015966),
+}
+table_size = 0.913
+cylinder_size = 0.21
+robot_size = 0.7
+
+
+def pose_calc(obj_str):
+    return coords.get(obj_str)[0] - coords.get('base')[0], coords.get(obj_str)[1] - coords.get('base')[1]
+
+
+def pose_calc_cyl(obj_str):
+    pose_table = list(pose_calc(obj_str))
+    pose_table[1] -= (cylinder_size + robot_size/2)
+    pose_table.append(0.68)
+    return tuple(pose_table)
+
+
+def pose_calc_table(angle):
+    pose_table = list(pose_calc('table'))
+    if angle == 1:
+        pose_table[0] += table_size
+        pose_table.append(-1.57)  # pi/2
+    if angle == 2:
+        pose_table[0] -= table_size
+        pose_table.append(1.57)
+    if angle == 3:
+        pose_table[1] += table_size
+        pose_table.append(0)
+    if angle == 4:
+        pose_table[1] -= table_size
+        pose_table.append(3.14)
+
+    return tuple(pose_table)
