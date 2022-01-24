0a21ca9e4413b16113204c8ca6c77b733bc63aed
gkiavash <gkiavash@gmail.com>
2022-01-24 18:18:32 +0100

    get_scene

--- tiago_iaslab_simulation/scripts/node_pick_place.py
+++ tiago_iaslab_simulation/scripts/node_pick_place.py
@@ -2,20 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import copy
 import rospy
+import rospkg
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 from actionlib import SimpleActionClient, SimpleActionServer
 
-from tiago_iaslab_simulation import msg as ir_msg
-
 from geometry_msgs import msg as geometry_msgs
 from moveit_msgs import msg as moveit_msgs
-# from apriltag_ros.msg import AprilTagDetectionArray
 import moveit_commander
-# from moveit_msgs.msg import CollisionObject
-# from moveit_msgs.srv import GetPositionIKRequest, GraspPlanning, GetPositionIK
-from math import pi
+
+from tiago_iaslab_simulation import msg as ir_msg
+from utils import *
 
 
 class PickAndPlaceServer(object):
@@ -62,15 +61,56 @@ class PickAndPlaceServer(object):
         self.lower_head()
         print('goal.all_objects_pose', len(goal.all_objects_pose))
         print('goal.all_objects_id', goal.all_objects_id)
+        self.get_scene(goal.all_objects_pose, goal.all_objects_id)
         self.move_arm(goal.object_pose)
         p_res = ir_msg.IRPickPlaceResult()
-        # error_code = self.grasp_object(goal.object_pose)
         error_code = 1
         p_res.error_code = error_code
         if error_code != 1:
             self.pick_as.set_aborted(p_res)
         else:
             self.pick_as.set_succeeded(p_res)
+
+    def get_scene(self, all_objects_pose, all_objects_id):
+        self.scene = moveit_commander.PlanningSceneInterface()
+
+        rospack = rospkg.RosPack()
+        file = rospack.get_path("tiago_iaslab_simulation") + "/meshes/hexagon.dae"
+
+        p = geometry_msgs.PoseStamped()
+        p.header.frame_id = "map"
+        p.pose = pose_calc('table')
+        self.scene.add_box("pick_table", p, (0.56, 0.56, 0.04))
+
+        name_to_id_cyl = {
+            1: 'place_table_b',
+            2: 'place_table_g',
+            3: 'place_table_r',
+        }
+        for cyl in name_to_id_cyl.keys():
+            p = geometry_msgs.PoseStamped()
+            p.header.frame_id = "map"
+            p.pose = pose_calc(str(cyl))
+            self.scene.add_cylinder("place_table_g", p, 0.21, 0.69)
+
+        name_to_id = {
+            1: 'Hexagon',
+            2: 'Triangle',
+            3: 'cube',
+            4: 'Obstacle0',
+            5: 'Obstacle1',
+            6: 'Obstacle2',
+            7: 'Obstacle3',
+        }
+        for obj_id_index, obj_id in enumerate(all_objects_id):
+            p = geometry_msgs.PoseStamped()
+            p.header.frame_id = "base_footprint"
+            p.pose = all_objects_pose[obj_id_index]
+            if obj_id == 3:
+                self.scene.add_box("cube", p, (0.05, 0.05, 0.05))
+                continue
+            self.scene.add_mesh(name_to_id[obj_id], p, file, (1, 1, 1))
+        return self.scene
 
     def move_arm(self, pose_stamped):
         moveit_commander.roscpp_initialize(sys.argv)
